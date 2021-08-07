b3516c6d5a89bcee4bb836cd490269166b0918a0
amin5560 <aminaminurrahman@gmail.com>
2021-08-07 23:08:30 +0600

    switch learning

--- Video-13/switch.js
+++ Video-13/switch.js
@@ -1,15 +1,15 @@
-num = 5
+num = 1000
 switch (num) {
     case 1000:
         console.log('I am 1000');
-    // break;
+        break;
     case 100:
         console.log('i am 100');
-    // break;
+        break;
     case 20:
     case 10:
         console.log('I am either 20 or 10');
-    // break;
+        break;
     default:
         console.log(`I do not know who are you`)
 }
