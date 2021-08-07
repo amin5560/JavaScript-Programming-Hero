bc2bab4487fa84bbe98b5cac60967c3a68cc10fa
amin5560 <aminaminurrahman@gmail.com>
2021-08-08 01:13:13 +0600

    function learning

--- Video-13/function.js
+++ Video-13/function.js
@@ -1,14 +1,26 @@
-// function sayLoveYou() {
-//     console.log('Say Love You');
-//     console.log('do not forgat')
-// }
-// sayLoveYou();
+function sayLoveYou() {
+    console.log('Say Love You');
+    console.log('do not forgat')
+}
+sayLoveYou();
 
+//double it use for bonus
 function doubleIt(num) {
     var result = num * 2;
+    // console.log(result);
     return result;
 }
+
 var first = doubleIt(5);
 var second = doubleIt(50);
 var total = first + second;
 console.log(total);
+
+
+function add(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+
+var sum = add(15, 17);
+console.log(sum);
