f03746d04acad884951de19a52eabe7538c8402c
amin5560 <aminaminurrahman@gmail.com>
2021-08-07 20:18:15 +0600

    foorloop learning

--- Video-13/forloop.js
+++ Video-13/forloop.js
@@ -1,16 +1,35 @@
+// while loop
 // var i = 10;
 // while (i <= 15) {
 //     console.log(i);
 //     i++;
 // }
 
-// for (var i = 0; i <= 10; i++) {
-//     console.log(i)
-// }
+// for loop
+// Structure:- Starting - Condition - Update
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i)
+}
 
 
 var nums = [55, 56, 57, 77, 88, 90, 11, 44];
 for (var i = 0; i < nums.length; i++) {
     var element = nums[i];
     console.log(element)
+}
+
+// print 1-50
+for (var x = 0; x <= 50; x = x + 1) {
+    console.log(x)
+}
+
+// even number 2,4,6....50
+for (var x = 0; x <= 50; x = x + 2) {
+    console.log(x)
+}
+
+// odd number 1,3,5....49
+for (var x = 1; x <= 49; x = x + 3) {
+    console.log(x)
 }
