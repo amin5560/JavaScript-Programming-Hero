222c6050ca57addebe3612d62d478cb84e1a42e0
amin5560 <aminaminurrahman@gmail.com>
2021-08-08 00:17:58 +0600

    Switch learning pard

--- Video-13/switch.js
+++ Video-13/switch.js
@@ -1,3 +1,4 @@
+// Structure -------- Switch, Case, Break, Default
 num = 1000
 switch (num) {
     case 1000:
@@ -12,4 +13,44 @@ switch (num) {
         break;
     default:
         console.log(`I do not know who are you`)
+}
+
+//digit spelling
+// 0 - Zero, 1 - one..... 9 - Nine, 10 - Not a valid digit
+// Switch, Case, Break, Default
+
+var digit = prompt("Enter any digit : ");
+switch (digit) {
+    case "0":
+        console.log("Zero");
+        break;
+    case "1":
+        console.log("One");
+        break;
+    case "2":
+        console.log("Two");
+        break;
+    case "3":
+        console.log("Three");
+        break;
+    case "4":
+        console.log("Four");
+        break;
+    case "5":
+        console.log("Five");
+        break;
+    case "6":
+        console.log("Six");
+        break;
+    case "7":
+        console.log("Seven");
+        break;
+    case "8":
+        console.log("Eight");
+        break;
+    case "9":
+        console.log("Nine");
+    default:
+        console.log("Not a digit");
+
 }
