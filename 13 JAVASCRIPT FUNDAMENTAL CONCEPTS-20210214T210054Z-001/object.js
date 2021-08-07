e656dde326c0a35c1e75b59e18b6ce5f0cefd75e
amin5560 <aminaminurrahman@gmail.com>
2021-08-08 03:15:30 +0600

    added number 14 tutorial folder

--- 13 JAVASCRIPT FUNDAMENTAL CONCEPTS-20210214T210054Z-001/object.js
+++ 13 JAVASCRIPT FUNDAMENTAL CONCEPTS-20210214T210054Z-001/object.js
@@ -17,3 +17,24 @@ student2.phone = 5589446;
 
 console.log(student);
 console.log(student2);
+
+///////////////////////////////////////
+//Student Details
+
+var name = "Aminur Rahman";
+var age = 27;
+var cgpa = 3.92;
+var lang = ["Bangla", "Hindi", "English"];
+
+//console.log(age);
+
+var student4 = {
+
+    name: "Aminur Rahman",
+    age: 27,
+    cgpa: 3.92,
+    lang: ["Bangla", "Hindi", "English"]
+
+}
+
+consol.log(student4.agr);
